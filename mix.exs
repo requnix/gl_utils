b1293297bb2785d26a82287ab0cce7675bda8fff
Sam Schneider <sds6065@gmail.com>
2016-05-24 18:03:39 -0400

    bumb version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WxUtils.Mixfile do
 
   def project do
     [app: :wx_utils,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      description: description,
      package: package,
